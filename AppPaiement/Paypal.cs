3c0bdc633fd2f3245597848a5749d27711100e85
THECODER <alphacanada7@gmail.com>
2024-10-24 02:49:08 -0400

    AJOUT CLASSE PAYPAL

---
+++ AppPaiement/Paypal.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppPaiement
+{
+    internal class Paypal : Paiement
+    {
+        public string Courriel { get; set; }
+
+        public Paypal(double montant, string description, string email) : base(montant, description)
+        {
+            Courriel = email;
+        }
+
+        // Implémentation de la méthode abstraite
+        public virtual void AfficherDetails()
+        {
+            Console.WriteLine($" PAYPAL : {Montant} , Description : {Description}, Courriel : {Courriel}");
+        }
+
+
+    }
+
+
+}
--- AppPaiement/Program.cs
+++ AppPaiement/Program.cs
@@ -8,14 +8,23 @@ internal class Program
 
 
         //CREATION INSTANCE
-        CarteCredit numcarte1 = new CarteCredit(10, "Achat de cahier", 54332458);
+        CarteCredit numcarte1 = new CarteCredit(10, "Achat de cahier   ", 54332458);
         CarteCredit numcarte2 = new CarteCredit(50, "Achat de vêtements", 565654321);
-        CarteCredit numcarte3 = new CarteCredit(29, "Achat Cmputer", 567890);
+        CarteCredit numcarte3 = new CarteCredit(29, "Achat Cmputer     ", 567890);
 
         // AFFICHAGE
         numcarte1.AfficherDetails();
         numcarte2.AfficherDetails();
         numcarte3.AfficherDetails();
 
+
+       Console.WriteLine();
+
+        Paypal paiement1 = new Paypal(76, "Achat produit  ", "alphacanada7@gmail.com");
+        Paypal paiement2 = new Paypal(41, "Achat materiaux", "alphacanada7@gmail.com");
+
+        
+        paiement1.AfficherDetails();
+        paiement2.AfficherDetails();
     }
 }
