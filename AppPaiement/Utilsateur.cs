ebdb6012c5a04aa85c031fe97a714f4462ad54c7
THECODER <alphacanada7@gmail.com>
2024-10-24 03:10:54 -0400

    AJOUT CLASSE UTILISATEUR

---
+++ AppPaiement/Utilsateur.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppPaiement
+{
+    internal class Utilsateur
+    {
+
+        public string Nom { get; set; }
+        public List<Paiement> Paiements { get; set; }
+
+        public Utilsateur(string nom)
+        {
+            Nom = nom;
+            Paiements = new List<Paiement>();
+        }
+
+        
+        public void AfficherInfos()
+        {
+            Console.WriteLine($"Nom de l'utilisateur : {Nom}");
+            Console.WriteLine("Paiements effectués :");
+            foreach (var paiement in Paiements)
+            {
+                paiement.AfficherDetails();
+            }
+        }
+
+        //  ajouter un paiement à la liste
+        public void AjouterPaiement(Paiement paiement)
+        {
+            Paiements.Add(paiement);
+        }
+    }
+}
